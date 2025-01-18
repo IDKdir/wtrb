1bf78e2a5473158193e06d82bd9e045ce2f4a27b
IDKdir <jysinok@gmail.com>
2025-01-18 13:55:57 -0500

    Hiya

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ local repos = 'https://raw.githubusercontent.com/IDKdir/wtrb/refs/heads/main'
 
 local function import(url)
     local s, e = pcall(function()
-        return loadstring(repos .. url)
+        return loadstring(game:HttpGet(repos .. url))()
     end)
     if not s then
         return warn('Could not import ', url)
